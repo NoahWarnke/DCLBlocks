eb3fc490cda520bede3aca29c154596c6fb6023c
Noah Warnke <noah.warnke@gmail.com>
2019-07-30 20:07:05 -0400

    Simplify the simple example Carryable class even further.

--- src/carryable-simple.ts
+++ src/carryable-simple.ts
@@ -1,41 +1,26 @@
-
 /**
- * A component that allows an entity to stay in the same location relative to a camera's position, when toggled.
+ * A component that allows an Entity to be carried, i.e. to
+ * stay in the same location relative to a user, when toggled.
  * @author Interweaver
  */
 @Component('carryable')
 export class Carryable {
   
-  public beingCarried: boolean;
-  public relativeTransform: Transform;
-  public camera: Camera;
-  
-  constructor() {
-    this.beingCarried = false;
-    this.relativeTransform = undefined;
-    this.camera = Camera.instance;
-  }
+  /**
+   * True when the Entity is being carried. False otherwise.
+   */
+  public beingCarried: boolean = false;
   
   /**
-   * Toggle whether this Carryable is being 'carried' (tracking user camera) or not.
-   * @param myTransform The Transform of the entity this Component was added to. No other good way to get this than passing it in here.
+   * When the Entity is being carried, its constant user-space position. Undefined otherwise.
    */
-  public toggleCarry(myTransform: Transform) {
+  public pUser: Vector3 = undefined;
+  
+  /**
+   * Toggle whether this Carryable is being carried or not.
+   */
+  public toggleCarry() {
     this.beingCarried = !this.beingCarried;
-    
-    if (!this.beingCarried) {
-      this.relativeTransform = undefined;
-      return;
-    }
-    
-    // Save position of object relative to camera at moment of toggle.
-    this.relativeTransform = new Transform({
-      position: myTransform.position
-        .subtract(this.camera.position)
-        .rotate(this.camera.rotation
-          .conjugate()
-        )
-    });
   }
 }
 
@@ -45,26 +30,55 @@ export class Carryable {
  */
 export class CarryableSystem {
   
+  /**
+   * The ComponentGroup of all Entities that have both Carryable and Transform Components.
+   */
   private carryables: ComponentGroup = engine.getComponentGroup(Carryable, Transform);
-  private camera: Camera;
   
-  constructor() {
-    this.camera = Camera.instance;
-  }
-  
+  /**
+   * Once per frame, update the position of each Entity that is being carried
+   * so that its position relative to the user remains constsant.
+   */
   public update(dt: number) {
+    
     for (let entity of this.carryables.entities) {
-      let carryable = entity.getComponent(Carryable);
-      if (!carryable.beingCarried) {
-        continue;
-      }
       
-      // Move object back to same relative position versus camera as when you clicked it.
-      entity.getComponent(Transform).position = carryable.relativeTransform.position
-        .clone()
-        .rotate(this.camera.rotation)
-        .add(this.camera.position)
-      ;
+      let carryable = entity.getComponent(Carryable);
+      
+      if (carryable.beingCarried) {
+        
+        if (carryable.pUser === undefined) {
+          
+          // Grab the current world-space position of the Entity.
+          let pWorld = entity.getComponent(Transform).position;
+          
+          // Calculate and save the user-space position of the Entity from its world-space position.
+          carryable.pUser = pWorld
+            .subtract(Camera.instance.position)           // Subtract U
+            .rotate(Camera.instance.rotation.conjugate()) // Unrotate by R
+          ;
+        }
+        else {
+          
+          // Calculate the variable world-space position of the Entity from its constant user-space position.
+          let pWorld = carryable.pUser
+            .clone()                          // (Clone stops the .rotate from changing pUser)
+            .rotate(Camera.instance.rotation) // Rotate by R
+            .add(Camera.instance.position)    // Add U
+          ;
+          
+          // Move the Entity to its new world-space position.
+          entity.getComponent(Transform).position = pWorld;
+        }
+      }
+      else if (carryable.pUser !== undefined) {
+        
+        // Entity was dropped, so don't care about pUser anymore.
+        carryable.pUser = undefined;
+      }
     }
   }
 }
+
+// Workaround for SDK bug with Camera.instance.
+let dummy = Camera.instance;
